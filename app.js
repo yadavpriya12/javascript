c309a883a415e728c88ee46b0fcf0e3b1f3d62c2
yadavpriya12 <169046727+yadavpriya12@users.noreply.github.com>
2024-12-04 21:59:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
-  
-// import express from "express"                                                                                                                                                                                                       import express from 'express'; 
-// import path from "path";
-// import { fileURLToPath } from "url"; 
-// const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// app.set('view engine', 'ejs');
-// app.set('Views', path.join(__dirname, 'Views')); 
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/Home', (req, res,next) => {
-//   res.render('Home'); 
-// });
-
-// app.get('/About', (req, res,next) => {
-//   res.render('About'); 
-// });
-
-// app.get('/Contact', (req, res,next) => {
-//   res.render('Contact'); 
-// });
-
-// app.listen(3002, () => {
-//   console.log('Server started');
-// });
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
